c3ee2d16ae481f37cb00084d0be2807faed2ffb6
Nizar Daffa <nizartokuzet@gmail.com>
2023-05-19 08:41:15 +0700

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">CRUD VUE 3 + Laravel 10</h1>
+        </div>
+    </div>
+</template>
---
+++ src/views/posts/index.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        Halaman Post Index
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
